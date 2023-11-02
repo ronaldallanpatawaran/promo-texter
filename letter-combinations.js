3ef7207ea68205ddb178fe8d0b13b938357b3a72
ronaldallanpatawaran <ronaldallanpatawaran@gmail.com>
2023-11-02 11:59:15 +0800

    Added explanation to the letter-combination.js

--- letter-combinations.js
+++ letter-combinations.js
@@ -10,30 +10,30 @@ function letterCombinations(digits) {
       '9': 'wxyz',
   };
 
-  function backtrack(index, currentCombination) {
-      if (index === digits.length) {
+    function backtrack(index, currentCombination) {
+      if (index === digits.length) { // if the current index will be equal to digits.length, the recursive call will stop and will return the output.
           if (currentCombination.length > 0) {
               combinations.push(currentCombination);
           }
           return;
       }
 
-      const digit = digits[index];
-      const letters = digitLetters[digit];
+      const digit = digits[index]; // current letter of the parameter digits, this digits is a string
+      const letters = digitLetters[digit]; // current set of letters depending on the selected or current digit of inputted parameters ( digits )
 
-      for (let i = 0; i < letters.length; i++) {
+      for (let i = 0; i < letters.length; i++) { // it will loop the current set of letters then it will call itself using recursive method, adding the index + 1 to get the next set of letters
           backtrack(index + 1, currentCombination + letters[i]);
       }
   }
 
   const combinations = [];
-  if (digits) {
+  if (digits) { // it will run only if the digits is not undefined
       backtrack(0, '');
   }
   return combinations;
 }
 
 // Example usage:
-console.log(letterCombinations("23")); // Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+console.log(letterCombinations("234")); // Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
 console.log(letterCombinations("")); // Output: []
 console.log(letterCombinations("2")); // Output: ["a","b","c"]
